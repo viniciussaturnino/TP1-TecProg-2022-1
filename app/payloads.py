73581c27c1b599448daf7c9c2942552323cf698d
Thiago <thiago099carvalho@gmail.com>
2022-07-15 11:40:14 -0300

    Removida falsificacao da classe ParkingLot e criada classe ParkingSystem
    Co-authored-by: Vinicius Saturnino <viniciussaturnino78@gmail.com>
    Co-authored-by: Mateus Gomes <mateusgomes.nascimento21@gmail.com>
    Co-authored-by: Paulo Victor <paulovmel@gmail.com>

--- app/payloads.py
+++ app/payloads.py
@@ -1,12 +1,27 @@
-class ParkingLot:
+class ParkingSystem:
     def __init__(self):
-        self.name = "1"
-        self.fraction_value = "1"
-        self.fulltime_value = "1"
-        self.daily_value_daytime = "1"
-        self.daily_value_overnight = "1"
-        self.subscription_access_value = "1"
-        self.event_access_value = "1"
-        self.opening_hours = "1"
-        self.capacity = "1"
-        self.contractor_percentage_revenue = "1"
+        self.parking_lots = []
+
+    def registerParkingLot(self, parking_lot):
+        self.parking_lots.append(parking_lot)
+
+class ParkingLot:
+    def __init__(self, payload: dict):
+        self.name = payload.get('name')
+        self.fraction_value = payload.get('fraction_value')
+        self.fulltime_value = payload.get('fulltime_value')
+        self.daily_value_daytime = payload.get('daily_value_daytime')
+        self.daily_value_overnight = payload.get('daily_value_overnight')
+        self.daily_overnight_initial_hour = payload.get('daily_overnight_initial_hour')
+        self.daily_overnight_end_hour = payload.get('daily_overnight_end_hour')
+        self.subscription_access_value = payload.get('subscription_access_value')
+        self.event_access_value = payload.get('event_access_value')
+        self.opening_hour = payload.get('opening_hour')
+        self.closing_hour = payload.get('closing_hour')
+        self.capacity = payload.get('capacity')
+        self.contractor_percentage_revenue = payload.get('contractor_percentage_revenue')
+
+class ParkingLotAccess:
+    def __init__(self):
+        self.checkin = '',
+        self.checkout = ''
--- tests/test_parking_lot.py
+++ tests/test_parking_lot.py
@@ -6,19 +6,22 @@ class TestCaseParkingLot(TestCase):
 
     def test_create_parking_lot(self) -> None:
         payload = dict(
-            name="1",
-            fraction_value="1",
-            fulltime_value="1",
-            daily_value_daytime="1",
-            daily_value_overnight="1",
-            subscription_access_value="1",
-            event_access_value="1",
-            opening_hours="1",
-            capacity="1",
-            contractor_percentage_revenue="1"
+            name = "Estac. 1",
+            fraction_value = 30,
+            fulltime_value = 15,
+            daily_value_daytime =  120,
+            daily_value_overnight = 45,
+            daily_overnight_initial_hour = 19,
+            daily_overnight_end_hour = 8,
+            subscription_access_value = 600,
+            event_access_value = 50,
+            opening_hour = 6,
+            closing_hour = 22,
+            capacity = 300,
+            contractor_percentage_revenue = 50
         )
 
-        self.parking_lot = ParkingLot()
+        self.parking_lot = ParkingLot(payload=payload)
 
         self.assertIsInstance(self.parking_lot, ParkingLot)
         self.assertEqual(self.parking_lot.__dict__, payload)
