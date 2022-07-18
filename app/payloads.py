a644d065b478597f9e84b970f86935da3da30e1c
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-07-18 15:18:50 -0300

    feat: implementação do tratamento de exceção de dados inválidos na criação de estacionamento
    Co-authored-by: Paulo Victor <paulovmel@gmail.com>
    Co-authored-by: Vinicius Saturnino <viniciussaturnino78@gmail.com>
    Co-authored-by: Thiago Mesquita <thiago099carvalho@gmail.com>

--- app/payloads.py
+++ app/payloads.py
@@ -1,4 +1,4 @@
-from app.exceptions import DescricaoEmBrancoException
+from app.exceptions import DescricaoEmBrancoException, ValorAcessoInvalidoException
 
 class ParkingLot:
     def __init__(self, payload: dict):
@@ -19,6 +19,23 @@ class ParkingLot:
           not 'contractor_percentage_revenue' in payload
         ):
           raise DescricaoEmBrancoException(payload=payload, type='estacionamento')
+        
+        # Tratamento de exceção de dados inválidos
+        if (
+          (type(payload.get('fraction_value')) != int or payload.get('fraction_value')<0) or
+          (type(payload.get('fulltime_value')) != int or payload.get('fulltime_value')<0) or
+          (type(payload.get('daily_value_daytime')) != int or payload.get('daily_value_daytime')<0) or
+          (type(payload.get('daily_value_overnight')) != int or payload.get('daily_value_overnight')<0) or
+          (type(payload.get('daily_overnight_initial_hour')) != int or payload.get('daily_overnight_initial_hour')<0) or
+          (type(payload.get('daily_overnight_end_hour')) != int or payload.get('daily_overnight_end_hour')<0) or
+          (type(payload.get('subscription_access_value')) != int or payload.get('subscription_access_value')<0) or
+          (type(payload.get('event_access_value')) != int or payload.get('event_access_value')<0) or
+          (type(payload.get('opening_hour')) != int or payload.get('opening_hour')<0) or
+          (type(payload.get('closing_hour')) != int or payload.get('closing_hour')<0) or
+          (type(payload.get('capacity')) != int or payload.get('capacity')<0) or
+          (type(payload.get('contractor_percentage_revenue')) != int or payload.get('contractor_percentage_revenue')<0)
+        ):
+          raise ValorAcessoInvalidoException(payload=payload, type='estacionamento')
         
         self.name = payload.get('name')
         self.fraction_value = payload.get('fraction_value')
