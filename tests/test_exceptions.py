435832ac9662c9ab66b7c59dc9e1071fa8502d15
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-07-18 15:09:43 -0300

    test: criação do teste de exceção para criação com dados inválidos de estacionamento

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,14 +1,14 @@
 from unittest import TestCase
 from app.payloads import ParkingLot
 
-from app.exceptions import DescricaoEmBrancoException
+from app.exceptions import DescricaoEmBrancoException, ValorAcessoInvalidoException
 
 
 class TesteExcecao(TestCase):
     def test_criacao_em_branco_de_estacionamento(self) -> None:
         self.assertRaises(DescricaoEmBrancoException, ParkingLot, {}) # Dados em branco
     
-    def test_entrada_em_branco_no_estacionamento(self):
+    def test_entrada_em_branco_no_estacionamento(self) -> None:
         payload = dict(
             name="Estac. 1",
             fraction_value=30,
@@ -27,3 +27,22 @@ class TesteExcecao(TestCase):
         self.parking_lot = ParkingLot(payload=payload)
         
         self.assertRaises(DescricaoEmBrancoException, self.parking_lot.register_parking_access, {}) # Dados de acesso em branco
+    
+    def test_criacao_com_dados_invalidos_de_estacionamento(self) -> None:
+        invalid_payload = dict(
+            name="Estac. 1",
+            fraction_value=-30,
+            fulltime_value=-15,
+            daily_value_daytime=- 120,
+            daily_value_overnight=-45,
+            daily_overnight_initial_hour=19.3,
+            daily_overnight_end_hour=-8,
+            subscription_access_value=-600,
+            event_access_value=-50,
+            opening_hour=-6,
+            closing_hour=-22,
+            capacity=-300,
+            contractor_percentage_revenue=-50.4
+        ) # Dados inválidos(negativos e não inteiros)
+        self.assertRaises(ValorAcessoInvalidoException, ParkingLot, invalid_payload)
+      
