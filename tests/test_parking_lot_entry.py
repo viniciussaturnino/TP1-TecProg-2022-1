b0ade805020842fbdd42d63ce654218b092998e3
Paulo Victor <paulovmel@gmail.com>
2022-07-18 07:47:34 -0300

    feat: parametriza entrada de veiculos no estacionamento
    Co-authored-by: Vinicius Saturnino <viniciussaturnino78@gmail.com>
    Co-authored-by: Mateus Gomes <mateusgomes.nascimento21@gmail.com>
    Co-authored-by: Thiago Mesquita <thiago099carvalho@gmail.com>

--- tests/test_parking_lot_entry.py
+++ tests/test_parking_lot_entry.py
@@ -1,9 +1,44 @@
 import pytest
-from unittest import TestCase
-from app.payloads import ParkingLot, ParkingSystem
+from app.payloads import ParkingLot
 
-class TestEntradasNoEstacionamento(TestCase):
-    def setUp(self):
+@pytest.mark.parametrize("parking_lot_entries", [
+    [
+        dict(
+            license_plate="HI139",
+            checkin='08:30:00',
+            checkout='08:56:00',
+        )
+    ],
+    [   dict(
+            license_plate="HI139",
+            checkin='08:30:00',
+            checkout='08:56:00'
+        ), 
+        dict(
+            license_plate="G49NG",
+            checkin='15:12:00',
+            checkout='16:00:00'
+        )
+    ],
+    [   dict(
+            license_plate="HI139",
+            checkin='08:30:00',
+            checkout='08:56:00'
+        ), 
+        dict(
+            license_plate="G49NG",
+            checkin='15:12:00',
+            checkout='16:00:00'
+        ),
+        dict(
+            license_plate="AC50M",
+            checkin='08:00:00',
+            checkout='18:00:00'
+        )
+    ],
+])
+class TestEntradasNoEstacionamento:        
+    def test_entradas_no_estacionamento(self, parking_lot_entries):
         payload = dict(
             name="Estac. 1",
             fraction_value=30,
@@ -20,36 +55,9 @@ class TestEntradasNoEstacionamento(TestCase):
             contractor_percentage_revenue=50
         )
         self.parking_lot = ParkingLot(payload=payload)
-        
 
-    def test_entrada_no_estacionamento(self):
-        payload = dict(
-            license_plate="HI139",
-            checkin='08:30:00',
-            checkout='08:56:00'
-        )
-        parking_access = self.parking_lot.register_parking_access(parking_access=payload)
+        for entrada in parking_lot_entries:
+            self.parking_lot.register_parking_access(parking_access=entrada)
 
-        assert parking_access == payload
-    
-    def test_entrada_no_estacionamento_b(self):
-        payload1 = dict(
-            license_plate="HI139",
-            checkin='08:30:00',
-            checkout='08:56:00'
-        )
-        payload2 = dict(
-            license_plate="HI139",
-            checkin='08:30:00',
-            checkout='08:56:00'
-        )
-
-        self.parking_lot.register_parking_access(parking_access=payload1)
-
-        parking_accesses = self.parking_lot.register_parking_access(parking_access=payload2)
-
-        assert parking_accesses[0] == payload1
-        assert parking_accesses[1] == payload2
-
-      
-      
+        parking_accesses = self.parking_lot.get_parking_accesses()
+        assert parking_accesses == parking_lot_entries
