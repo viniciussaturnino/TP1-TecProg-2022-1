d3cb67346d5638686beaa055ccf05f8ae3c780fa
Paulo Victor <paulovmel@gmail.com>
2022-07-18 06:48:12 -0300

    feat: cria caso de teste para entrada no estacionamento
    Co-authored-by: Vinicius Saturnino <viniciussaturnino78@gmail.com>
    
    Co-authored-by: Mateus Gomes <mateusgomes.nascimento21@gmail.com>
    Co-authored-by: Thiago Mesquita <thiago099carvalho@gmail.com>

---
+++ tests/test_parking_lot_entry.py
@@ -0,0 +1,36 @@
+import pytest
+from unittest import TestCase
+from app.payloads import ParkingLot, ParkingSystem
+
+class TestEntradasNoEstacionamento(TestCase):
+    def setUp(self):
+        payload = dict(
+            name="Estac. 1",
+            fraction_value=30,
+            fulltime_value=15,
+            daily_value_daytime= 120,
+            daily_value_overnight=45,
+            daily_overnight_initial_hour=19,
+            daily_overnight_end_hour=8,
+            subscription_access_value=600,
+            event_access_value=50,
+            opening_hour=6,
+            closing_hour=22,
+            capacity=300,
+            contractor_percentage_revenue=50
+        )
+        self.parking_lot = ParkingLot(payload=payload)
+        
+
+    def test_entrada_no_estacionamento(self):
+        payload = dict(
+            license_plate="HI139",
+            checkin='08:30:00',
+            checkout='08:56:00'
+        )
+        parking_accesses = self.parking_lot.register_parking_access(parking_access=payload)
+
+        assert parking_accesses[0] == payload
+
+      
+      
