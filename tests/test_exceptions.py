3762fab0fc04ed57012fbcd0ea4ad15c2bd9097b
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-07-18 14:51:41 -0300

    test: criação do teste de exceção de dados em branco para o acesso ao estacionamento

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -5,5 +5,25 @@ from app.exceptions import DescricaoEmBrancoException
 
 
 class TesteExcecao(TestCase):
-    def test_create_blank_parking_lot(self) -> None:
+    def test_criacao_em_branco_de_estacionamento(self) -> None:
         self.assertRaises(DescricaoEmBrancoException, ParkingLot, {}) # Dados em branco
+    
+    def test_entrada_em_branco_no_estacionamento(self):
+        payload = dict(
+            name="Estac. 1",
+            fraction_value=30,
+            fulltime_value=15,
+            daily_value_daytime= 120,
+            daily_value_overnight=45,
+            daily_overnight_initial_hour=19,
+            daily_overnight_end_hour=8,
+            subscription_access_value=600,
+            event_access_value=50,
+            opening_hour=6,
+            closing_hour=22,
+            capacity=300,
+            contractor_percentage_revenue=50
+        )
+        self.parking_lot = ParkingLot(payload=payload)
+        
+        self.assertRaises(DescricaoEmBrancoException, self.parking_lot.register_parking_access, {}) # Dados de acesso em branco
