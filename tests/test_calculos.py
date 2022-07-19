a42dc93439646d764f8013728566490c4def6933
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-07-18 23:29:29 -0300

    [Duplicação]: criação do teste de duplicação para cálculo de hora cheia
    Co-authored-by: Vinicius Saturnino <viniciussaturnino78@gmail.com>
    Co-authored-by: Paulo Victor <paulovmel@gmail.com>
    Co-authored-by: Thiago Mesquita <thiago099carvalho@gmail.com>

--- tests/test_calculos.py
+++ tests/test_calculos.py
@@ -30,3 +30,33 @@ class TestCalculos:
         price = parking_lot.register_parking_access(parking_access=parking_lot_entry)
         
         assert price == parking_lot_entry['expected_price']
+        
+    def test_calculo_hora_cheia_2(self):
+        payload = dict(
+            name="Estac. 1",
+            fraction_value=30,
+            fulltime_value=15,
+            daily_value_daytime= 120,
+            daily_value_overnight=45,
+            daily_overnight_initial_hour="19:00:00",
+            daily_overnight_end_hour="08:00:00",
+            subscription_access_value=600,
+            event_access_value=50,
+            opening_hour="08:00:00",
+            closing_hour="19:00:00",
+            capacity=300,
+            contractor_percentage_revenue=50
+        )
+        parking_lot = ParkingLot(payload=payload)
+        
+        parking_lot_entry = dict(
+            license_plate="PB139",
+            checkin="10:00:00",
+            checkout="11:00:00",
+            type = None,
+            expected_price = 102
+        )
+        
+        price = parking_lot.register_parking_access(parking_access=parking_lot_entry)
+        
+        assert price == parking_lot_entry['expected_price']
