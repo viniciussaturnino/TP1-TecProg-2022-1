cc05661fca48f71d2e6f41d87fdc85aba48890e4
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-07-18 23:55:40 -0300

    [Triangulação]: Criação do teste com triangulação e parametrização para cálculo de acesso noturno
    Co-authored-by: Vinicius Saturnino <viniciussaturnino78@gmail.com>
    Co-authored-by: Paulo Victor <paulovmel@gmail.com>
    Co-authored-by: Thiago Mesquita <thiago099carvalho@gmail.com>

--- tests/test_calculos.py
+++ tests/test_calculos.py
@@ -58,38 +58,37 @@ class TestCalculoHoraCheia:
         for acesso in parking_accesses:
             assert acesso['price'] == acesso['expected_price']
 
-class TestCalculoAcessoNoturno:
-    def test_calculo_acesso_noturno(self):
-        payload = dict(
-            name="Estac. 1",
-            fraction_value=30,
-            fulltime_value=15,
-            daily_value_daytime= 120,
-            daily_value_overnight=45,
-            daily_overnight_initial_hour="19:00:00",
-            daily_overnight_end_hour="08:00:00",
-            subscription_access_value=600,
-            event_access_value=50,
-            opening_hour="08:00:00",
-            closing_hour="23:00:00",
-            capacity=300,
-            contractor_percentage_revenue=50
-        )
-        parking_lot = ParkingLot(payload=payload)
-
-        parking_acess = dict(
+@pytest.mark.parametrize("parking_lot_entries", [
+    [
+        dict(
             license_plate="V49NG",
             checkin="20:00:00",
             checkout="22:00:00",
             type = None,
             expected_price = 204
         )
-        
-        price = parking_lot.register_parking_access(parking_access=parking_acess)
-
-        assert price == parking_acess['expected_price']
-        
-    def test_calculo_acesso_noturno_2(self):
+    ],
+    [   dict(
+            license_plate="PB139",
+            checkin="20:00:00",
+            checkout="21:00:00",
+            type = None,
+            expected_price = 102
+        )
+    ],
+    [
+        dict(
+            license_plate="G49NG",
+            checkin="19:30:00",
+            checkout="22:30:00",
+            type = None,
+            expected_price = 306
+        )
+    ],
+  ]
+)
+class TestCalculoAcessoNoturno:
+    def test_calculo_acesso_noturno(self, parking_lot_entries):
         payload = dict(
             name="Estac. 1",
             fraction_value=30,
@@ -107,14 +106,11 @@ class TestCalculoAcessoNoturno:
         )
         parking_lot = ParkingLot(payload=payload)
 
-        parking_acess = dict(
-            license_plate="V49NG",
-            checkin="20:00:00",
-            checkout="21:00:00",
-            type = None,
-            expected_price = 102
-        )
-        
-        price = parking_lot.register_parking_access(parking_access=parking_acess)
+        for entrada in parking_lot_entries:
+            parking_lot.register_parking_access(parking_access=entrada)
 
-        assert price == parking_acess['expected_price']
+        parking_accesses = parking_lot.get_parking_accesses()
+        assert parking_accesses == parking_lot_entries
+
+        for acesso in parking_accesses:
+            assert acesso['price'] == acesso['expected_price']
