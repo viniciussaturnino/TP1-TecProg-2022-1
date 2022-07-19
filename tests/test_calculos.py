f1b7da0138bdc3e428aa60764d58c28f46c1f806
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-07-18 23:49:36 -0300

    [Falsificação]: criação do teste de acesso noturno utilizando a falsificação
    Co-authored-by: Vinicius Saturnino <viniciussaturnino78@gmail.com>
    Co-authored-by: Paulo Victor <paulovmel@gmail.com>
    Co-authored-by: Thiago Mesquita <thiago099carvalho@gmail.com>

--- tests/test_calculos.py
+++ tests/test_calculos.py
@@ -30,7 +30,7 @@ from app.payloads import ParkingLot
     ],
   ]
 )
-class TestCalculos:            
+class TestCalculoHoraCheia:            
     def test_calculo_hora_cheia(self, parking_lot_entries):
         payload = dict(
             name="Estac. 1",
@@ -57,3 +57,34 @@ class TestCalculos:
 
         for acesso in parking_accesses:
             assert acesso['price'] == acesso['expected_price']
+
+class TestCalculoAcessoNoturno:
+    def test_calculo_acesso_noturno(self):
+        payload = dict(
+            name="Estac. 1",
+            fraction_value=30,
+            fulltime_value=15,
+            daily_value_daytime= 120,
+            daily_value_overnight=45,
+            daily_overnight_initial_hour="19:00:00",
+            daily_overnight_end_hour="08:00:00",
+            subscription_access_value=600,
+            event_access_value=50,
+            opening_hour="08:00:00",
+            closing_hour="23:00:00",
+            capacity=300,
+            contractor_percentage_revenue=50
+        )
+        parking_lot = ParkingLot(payload=payload)
+
+        parking_acess = dict(
+            license_plate="V49NG",
+            checkin="20:00:00",
+            checkout="22:00:00",
+            type = None,
+            expected_price = 204
+        )
+        
+        price = parking_lot.register_parking_access(parking_access=parking_acess)
+
+        assert price == parking_acess['expected_price']
