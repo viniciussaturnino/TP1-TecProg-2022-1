e07e2de58c50429ce8ceddf9960ca90595494c5b
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-09-01 00:31:00 -0300

    Refactoring: extrair constantes Mau-cheiro identificado: números mágicos Descrição da refatoração: aplicou-se a refatoração para extrair os números mágicos do código em constantes. Classes/métodos/atributos afetados: * Método get_parking_access_price_by_time teve constantes criadas para os seus números mágicos
    Co-authored-by: Vinicius Saturnino <viniciussaturnino78@gmail.com>
    Co-authored-by: Paulo Victor <paulovmel@gmail.com>
    Co-authored-by: Thiago Mesquita <thiago099carvalho@gmail.com>

--- app/parking_access.py
+++ app/parking_access.py
@@ -1,24 +1,32 @@
 from datetime import datetime
 
+# Constantes
+PERCENT_CONVERSION_VALUE = 100
+HOUR_IN_SECONDS = 3600
+MINUTES_IN_HOUR = 60
+FRACTIONS_PER_HOUR = 4
+MAX_DAYTIME_HOURS = 9
+FRACTION_IN_MINUTES = 15
+
 
 class ParkingAccess:
   
-  def get_price_by_time(self, parking_access: dict, parking_lot: object):
-        checkin = datetime.strptime(parking_access.get('checkin'), "%H:%M:%S")
-        checkout = datetime.strptime(parking_access.get('checkout'), "%H:%M:%S")
+    def get_price_by_time(self, parking_access: dict, parking_lot: object):
+          checkin = datetime.strptime(parking_access.get('checkin'), "%H:%M:%S")
+          checkout = datetime.strptime(parking_access.get('checkout'), "%H:%M:%S")
 
-        if checkin >= datetime.strptime(parking_lot.daily_overnight_initial_hour, "%H:%M:%S"
-        ) and checkout <= datetime.strptime(parking_lot.daily_overnight_end_hour, "%H:%M:%S"):
-            return parking_lot.daily_value_daytime*(parking_lot.daily_value_overnight/100)
-        
-        total_seconds = (checkout-checkin).seconds
-        hours = int(total_seconds/3600)
+          if checkin >= datetime.strptime(parking_lot.daily_overnight_initial_hour, "%H:%M:%S"
+          ) and checkout <= datetime.strptime(parking_lot.daily_overnight_end_hour, "%H:%M:%S"):
+              return parking_lot.daily_value_daytime*(parking_lot.daily_value_overnight/PERCENT_CONVERSION_VALUE)
+          
+          total_seconds = (checkout-checkin).seconds
+          hours = int(total_seconds/HOUR_IN_SECONDS)
 
-        if hours >= 9:
-            return parking_lot.daily_value_daytime
+          if hours >= MAX_DAYTIME_HOURS:
+              return parking_lot.daily_value_daytime
 
-        minutes = int((total_seconds - (hours*3600))/60)
-        hours_value = int(hours*((4*parking_lot.fraction_value)*(1 - (parking_lot.fulltime_value/100))))
-        minutes_value = int(minutes/15)*parking_lot.fraction_value
+          minutes = int((total_seconds - (hours*HOUR_IN_SECONDS))/MINUTES_IN_HOUR)
+          hours_value = int(hours*((FRACTIONS_PER_HOUR*parking_lot.fraction_value)*(1 - (parking_lot.fulltime_value/PERCENT_CONVERSION_VALUE))))
+          minutes_value = int(minutes/FRACTION_IN_MINUTES)*parking_lot.fraction_value
 
-        return hours_value+minutes_value
+          return hours_value+minutes_value
